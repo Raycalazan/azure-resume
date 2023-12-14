742aeade9f93e1b58268d279173f520bb91f6e52
raycalazan <raymond.calazan@gmail.com>
2023-12-14 14:36:19 -0500

    trying to change jquery to https

---
+++ frontend/js/main.js
@@ -0,0 +1,20 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    getVisitCount();
+});
+
+const functionAPI = 'https://ray-resume-function.azurewebsites.net/api/HttpTrigger1?';
+
+const getVisitCount = () => {
+    let count = 30;
+    fetch(functionAPI)
+        .then(response => response.json())
+        .then(response => {
+            console.log("Website called function API.");
+            count = response.count;
+            document.getElementById("counter").innerText = count;
+        })
+        .catch(error => {
+            console.log('Error fetching data:', error);
+            document.getElementById("counter").innerText = count;
+        });
+}
