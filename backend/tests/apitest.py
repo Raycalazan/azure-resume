9a30b167da7b85ee6a5f5c71e1f668ffc156cbe2
Raymond Calazan <rcalazan@Raymonds-Air.localdomain>
2024-02-09 19:18:01 -0500

    updated test to use new cosmosdb

--- backend/tests/apitest.py
+++ backend/tests/apitest.py
@@ -1,25 +1,25 @@
 import os
-
-
+from azure.identity import DefaultAzureCredential
 from azure.core.exceptions import AzureError
 from azure.cosmos import CosmosClient, PartitionKey
 
 
-CONN_STR = os.environ["COSMOS_CONNECTION_STRING"]
-client = CosmosClient.from_connection_string(CONN_STR)
+URI = "https://tf-ray-resume-cosmosdb.documents.azure.com:443/"
+KEY = "3voBEaObThaXVAv2Kv9JHc28GY6ljoy4LvXrIhCV96SwbGVQRS1yE3mtIOuoDX69k39TR73vrHZTACDbthewog=="
+client = CosmosClient(URI,KEY)
 
-database_ID="my-database"
+database_ID="tf-resume-db"
 
 database=client.get_database_client(database_ID)
 
-container_id= "my-container"
+container_id= "db-container"
 container=database.get_container_client(container_id)
 
 item_id_to_read = "Azure"
 
 item= container.read_item(item_id_to_read,item_id_to_read)
 
-original_count=item["count"]
+original_count=item["Count"]
 
 new_count=original_count+1
 
